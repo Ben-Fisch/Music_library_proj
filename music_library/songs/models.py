ee6267641132537e187c558f37cabd4dd05ebdb1
Ben Fischer <fischerben010@gmail.com>
2021-10-29 18:43:02 -0500

    Add genre as a field and set up to be able to view a single song

--- music_library/songs/models.py
+++ music_library/songs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
 class Song(models.Model):
@@ -6,3 +7,4 @@ class Song(models.Model):
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
     release_date = models.DateTimeField()
+    genre = models.CharField(default="r&b", max_length=50)
--- music_library/songs/urls.py
+++ music_library/songs/urls.py
@@ -1,6 +1,9 @@
+from django.http import request
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('songs/', views.SongList.as_view()),
+    
+    path('songs/<int:pk>/', views.SongDetail.as_view())
 ]
