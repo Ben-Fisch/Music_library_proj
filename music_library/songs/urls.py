c63eeeadfaf2217710c41539f3a0ce5d90e0856e
Ben Fischer <fischerben010@gmail.com>
2021-10-31 21:54:47 -0500

    Add delete and edit

--- music_library/songs/urls.py
+++ music_library/songs/urls.py
@@ -2,8 +2,8 @@ from django.http import request
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('songs/', views.SongList.as_view()),
-    
     path('songs/<int:pk>/', views.SongDetail.as_view())
 ]
